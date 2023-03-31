2e7ac2d5ec3eaf76de33f750c6aad3bfcb2d8e29
Rodney Odundo <orodney10@gmail.com>
2023-03-31 22:52:28 +0300

    first commit

---
+++ forms.js
@@ -0,0 +1,51 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+
+import { getFirestore, addDoc,collection} from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWov7boQ9ZniID8kLR7GiWv8Ci9t2-JN0",
+    authDomain: "spareparts-a2d26.firebaseapp.com",
+    projectId: "spareparts-a2d26",
+    storageBucket: "spareparts-a2d26.appspot.com",
+    messagingSenderId: "677086045841",
+    appId: "1:677086045841:web:16fbd4bedc4a9daaaf0130",
+    measurementId: "G-QDELWPR77Z"
+  };
+
+
+ const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+      const db = getFirestore(app);
+
+
+
+const submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener('click', (event) => {
+    const brand = document.getElementById('brand').value;
+    const model = document.getElementById('model').value;
+    const year = document.getElementById('year').value;
+    const spareparts = document.getElementById('spareparts').value;
+    var name = document.getElementById('name').value;
+    var address = document.getElementById('address').value;
+    var telephone = document.getElementById('phone').value;
+
+
+    addDoc(collection(db, "orders"),{
+        Brand: brand,
+        Model: model,
+        Year: year,
+        Spareparts: spareparts,
+        Name: name,
+        Address: address,
+        Telephone: telephone
+
+
+    });
+    
+    alert('Order has been placed');
+});
+
---
+++ login.js
@@ -0,0 +1,130 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import {
+  getDatabase,
+  set,
+  ref,
+  child,
+  get,
+} from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
+// import { db } from "./forms.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWov7boQ9ZniID8kLR7GiWv8Ci9t2-JN0",
+    authDomain: "spareparts-a2d26.firebaseapp.com",
+    projectId: "spareparts-a2d26",
+    storageBucket: "spareparts-a2d26.appspot.com",
+    messagingSenderId: "677086045841",
+    appId: "1:677086045841:web:16fbd4bedc4a9daaaf0130",
+    measurementId: "G-QDELWPR77Z"
+  };
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+
+
+
+const submitButton = document.getElementById("submit");
+const signupButton = document.getElementById("sign-up");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const main = document.getElementById("main");
+const createacct = document.getElementById("create-acct")
+
+const signupEmailIn = document.getElementById("email-signup");
+const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
+const signupPasswordIn = document.getElementById("password-signup");
+const confirmSignUpPasswordIn = document.getElementById("confirm-password-signup");
+const createacctbtn = document.getElementById("create-acct-btn");
+
+const returnBtn = document.getElementById("return-btn");
+
+var email, password, signupEmail, signupPassword, confirmSignupEmail, confirmSignUpPassword;
+
+createacctbtn.addEventListener("click", function() {
+  var isVerified = true;
+
+  signupEmail = signupEmailIn.value;
+  confirmSignupEmail = confirmSignupEmailIn.value;
+  if(signupEmail != confirmSignupEmail) {
+      window.alert("Email fields do not match. Try again.")
+      isVerified = false;
+  }
+
+  signupPassword = signupPasswordIn.value;
+  confirmSignUpPassword = confirmSignUpPasswordIn.value;
+  if(signupPassword != confirmSignUpPassword) {
+      window.alert("Password fields do not match. Try again.")
+      isVerified = false;
+  }
+  
+  if(signupEmail == null || confirmSignupEmail == null || signupPassword == null || confirmSignUpPassword == null) {
+    window.alert("Please fill out all required fields.");
+    isVerified = false;
+  }
+  
+  if(isVerified) {
+    createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+      .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      
+
+      //  addDoc(collection(db, "users"),{
+      //    Email: signupEmailIn,
+      
+      // });
+  
+      window.alert("Success! Account created.");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+      window.alert("Error occurred. Try again.");
+    });
+  }
+});
+
+submitButton.addEventListener("click", function() {
+  email = emailInput.value;
+  console.log(email);
+  password = passwordInput.value;
+  console.log(password);
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log("Success! Welcome back!");
+      window.alert("Success! Welcome back!");
+      window.location.href = "homepage.html";
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Error occurred. Try again.");
+      window.alert("Error occurred. Try again.");
+    });
+});
+
+signupButton.addEventListener("click", () => {
+  main.style.display = 'none'
+  createacct.style.display = 'block'
+  
+}
+)
+
+returnBtn.addEventListener("click", function() {
+    main.style.display = "block";
+    createacct.style.display = "none";
+});
+
